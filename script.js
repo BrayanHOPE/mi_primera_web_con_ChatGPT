449c7cf507991cbb6c2922dca79976aa7d0e043b
BrayanHOPE <brayan.huanque2002@gmail.com>
2024-03-13 20:55:36 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleDescription(element) {
+    const description = element.querySelector('.description');
+    description.style.display = description.style.display === 'none' ? 'flex' : 'none';
+}
